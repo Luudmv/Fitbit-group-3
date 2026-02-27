6cb13ef3bb1de027d8193d6863b77bbfab359d11
Luudmv <l.m.vermeeren@student.vu.nl>
2026-02-27 16:50:26 +0100

    Update part3.py

--- scripts/part3.py
+++ scripts/part3.py
@@ -1,1 +1,17 @@
+import sqlite3
 
+con = sqlite3.connect("fitbit_database.db")
+cur = con.cursor()
+
+# res = cur.execute("SELECT name FROM sqlite_master")
+# print(res.fetchall())
+
+# for row in cur.execute("SELECT * FROM hourly_steps"):
+#     print(row)
+
+cur.execute("SELECT * FROM hourly_steps")
+column_names = [description[0] for description in cur.description]
+
+print(column_names)
+
+con.close()
